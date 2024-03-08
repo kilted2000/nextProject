a1d104d2c9caa550fb7302da7a72ffecf4dae7af
Richard <richardlwhittington@gmail.com>
2024-03-07 22:09:01 -0600

    add changes

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+   <div className="bg-rose-950 h-screen">
+   <h1 className="h-8 italic text-sky-600">
+   Hello Next</h1>
+   <p className="text-slate-50">How are you doing?</p>
+   <Image src="crests.jpeg" alt="" />
+   </div>
+  );
+}
