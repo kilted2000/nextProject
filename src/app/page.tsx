fa894c3ff0dc52c71d04acb9d42427ae1ac4d840
Richard <richardlwhittington@gmail.com>
2024-03-09 17:21:32 -0600

    fix image from next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
    <h1 className="h-8 italic text-sky-600">
    Hello Next</h1>
    <p className="text-slate-50">How are you doing?</p>
-   <Image src="crests.jpeg" alt="" />
+   <Image src="/crests.jpeg" alt="" width={500} height={550}/>
    </div>
   );
 }
