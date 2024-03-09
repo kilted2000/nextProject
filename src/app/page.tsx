6d79f61496869d6b049c2696266e792797281c2b
Richard <richardlwhittington@gmail.com>
2024-03-09 17:28:05 -0600

    change languages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
    <div className="bg-rose-950 h-screen">
    <h1 className="h-8 italic text-sky-600">
-   Hello Next</h1>
-   <p className="text-slate-50">How are you doing?</p>
+   Halo Next</h1>
+   <p className="text-slate-50">Ciamar a tha sibh?</p>
    <Image src="/crests.jpeg" alt="" width={500} height={550}/>
    </div>
   );
