dff15a09f0a6074be6d4f4925a37d16695edefee
Richard <richardlwhittington@gmail.com>
2024-03-09 23:15:33 -0600

    add today

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
    <div className="bg-rose-950 h-screen">
    <h1 className="h-8 italic text-sky-600">
    Halo A Charaidean</h1>
-   <p className="text-slate-50">Ciamar a tha sibh?</p>
+   <p className="text-slate-50">Ciamar a tha sibh an-dugh?</p>
    <Image src="/crests.jpeg" alt="" width={500} height={550}/>
    </div>
   );
